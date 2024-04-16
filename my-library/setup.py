b92282b45671b88685ec70560263d3d39e1b75a9
Edgar Raul Cusi Osccorima <104920177+CusiTEC@users.noreply.github.com>
2024-04-16 11:14:26 -0500

    Create setup.py

---
+++ my-library/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="my-library",
+    version="0.1.0",
+    description="Una biblioteca simple para saludar a alguien.",
+    author="CusiTEC",
+    author_email="edgarraulcusiosccorima@gmail.com",
+    packages=["my_library"],
+    install_requires=[],
+)
